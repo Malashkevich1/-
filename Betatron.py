8f8dd303fbfa333a0148b17bd7288105e14c9391
Malashkevich1 <35844506+Malashkevich1@users.noreply.github.com>
2018-03-16 22:36:08 +0300

    Add files via upload

--- Betatron.py
+++ Betatron.py
@@ -1,6 +1,9 @@
 import math 
 import pygame, sys 
 from pygame.locals import * 
+
+#x=r*cos(a); y=r*sin(a); a=wt; t 
+
 pygame.init() 
 
 (windows_width, windows_height, windows_title) = (1000, 768, "Betatron") 
@@ -19,9 +22,16 @@ circle_2_pos = (500,384)
 circle_2_radius = 250 
 circle_2_width = 5
 circle_3_color = (0,0,255) 
-circle_3_pos = (500,209) 
+circle_3_pos = (500,384) 
 circle_3_radius = 10 
 circle_3_width = 0
+
+a = 0.00000001
+x = 0
+y = 0
+r = 200
+t = 0
+w = 0.001
 
 while mainLoop: 
   for event in pygame.event.get(): 
@@ -32,8 +42,12 @@ while mainLoop:
   #create frame here 
   pygame.draw.circle(screen, circle_1_color, circle_1_pos, circle_1_radius, circle_1_width)
   pygame.draw.circle(screen, circle_2_color, circle_2_pos, circle_2_radius, circle_2_width)
-  pygame.draw.circle(screen, circle_3_color, circle_3_pos, circle_3_radius, circle_3_width)
+  pygame.draw.circle(screen, circle_3_color, (circle_3_pos[0] + x, circle_3_pos[1] + y), circle_3_radius, circle_3_width)
+  w += a*t
+  t += 1
+  x = r*math.cos(w*t)
+  y = r*math.sin(w*t)
   pygame.display.update() 
-
+  
 pygame.quit() 
 #destroy data here 
