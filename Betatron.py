8a1bcd2da79d7e0af8d3b3b28783693a507e0402
Malashkevich1 <35844506+Malashkevich1@users.noreply.github.com>
2018-03-18 23:17:11 +0300

    Add files via upload

--- Betatron.py
+++ Betatron.py
@@ -38,14 +38,23 @@ circle_5_width = 0
 textpos_1 = (10,10)
 textpos_2 = (10,40)
 textpos_3 = (10,70)
+textpos_4 = (10,100)
+textpos_5 = (10,130)
 
-a = 0.00000001
+b = 0
+E = 0
 x = 0
 y = 0
-r = 200
 t = 0
-w = 0.001
+w = 0
 v = 0
+
+#const
+
+r = 200
+a = 10
+c = 299792458
+m_0 = 9.10938356 * 10**-31
 
 while mainLoop: 
   for event in pygame.event.get(): 
@@ -55,22 +64,29 @@ while mainLoop:
   screen.fill(windows_bgcolor) 
 
   #create frame here
-  text_1 = font.render("Time = " + str(t), True, [0, 0, 0])
-  text_2 = font.render("Acceleration = " + str(a), True, [0, 0, 0])
-  text_3 = font.render("Speed = " + str(v), True, [0, 0, 0])
+  text_1 = font.render("Time (s) = " + str(t), True, [0, 0, 0])
+  text_2 = font.render("Acceleration (mps^2) = " + str(a), True, [0, 0, 0])
+  text_3 = font.render("Speed (mps)  = " + str(v), True, [0, 0, 0])
+  text_4 = font.render("Energy (MeV)  = " + str(E), True, [0, 0, 0])
+  text_5 = font.render("Electron mass (kg)  = " + str(m_0), True, [0, 0, 0])
   screen.blit(text_1, textpos_1)
   screen.blit(text_2, textpos_2)
   screen.blit(text_3, textpos_3)
+  screen.blit(text_4, textpos_4)
+  screen.blit(text_5, textpos_5)
   pygame.draw.circle(screen, circle_1_color, circle_1_pos, circle_1_radius, circle_1_width)
   pygame.draw.circle(screen, circle_2_color, circle_2_pos, circle_2_radius, circle_2_width)
   pygame.draw.circle(screen, circle_3_color, (circle_3_pos[0] + x, circle_3_pos[1] + y), circle_3_radius, circle_3_width)
   pygame.draw.circle(screen, circle_4_color, (circle_4_pos[0] + x, circle_4_pos[1] + y), circle_4_radius, circle_4_width)
   pygame.draw.circle(screen, circle_5_color, (circle_5_pos[0] + x, circle_5_pos[1] + y), circle_5_radius, circle_5_width)
-  w += a*t
-  t += 0.5
+  t += 0.0022
+  v += a*t
+  b = v/c
+  E = (m_0*c*c/math.sqrt(1-b*b-m_0*c*c))*6.241506363094 * 10**12
+  w += v/r*0.000264
   x = r*math.cos(w*t)
   y = r*math.sin(w*t)
-  v = w*r
+  
   pygame.display.update() 
   
 pygame.quit() 
